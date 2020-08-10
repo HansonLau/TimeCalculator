74a982bc734d0e0ebd26255f5da5b4b2d1d541a7
HansonLau <shadowguy368@gmail.com>
2020-08-10 14:52:38 -0700

    first commint

---
+++ TimeCalculator/src/calculate.java
@@ -0,0 +1,100 @@
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.time.LocalDateTime;
+
+import javax.swing.*;
+
+
+
+public class calculate extends JFrame{
+
+	final private int WIDTH = 500;
+	final private int HEIGHT = 400;
+	private JButton backward = new JButton("Backward");
+	private JButton forward = new JButton("Forward");
+	
+	ClickListener cl = new ClickListener();
+	
+	public calculate(){
+	
+		
+		JPanel panel = new JPanel();
+		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+		this.setLocation((dim.width/2-this.getSize().width/2)-WIDTH/2, 
+				(dim.height/2-this.getSize().height/2)-HEIGHT/2);
+		this.setSize(WIDTH, HEIGHT);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setTitle("TimeCalculator");
+		panel.setLayout(null);
+		
+		
+		backward.setBounds(20, 150, 200, 50);
+		forward.setBounds(270, 150, 200, 50);
+		
+		panel.add(backward);
+		panel.add(forward);
+		backward.addActionListener(cl);
+		forward.addActionListener(cl);
+		
+		this.add(panel);
+		this.setVisible(true);	
+		
+	}
+	
+	
+	private void findTime(String action) {
+		
+		int hours = Integer.parseInt(JOptionPane.showInputDialog("How many hours " + action +"?"));
+		int minutes = Integer.parseInt(JOptionPane.showInputDialog("How many minutes " + action +"?"));
+		
+		
+		if(action.equals("forward")) {
+			int findHours = (LocalDateTime.now().getHour() + hours) % 12;
+			int findMinutes = (LocalDateTime.now().getMinute() + minutes);
+			
+			if(findMinutes > 60) {
+				findHours++;
+				findMinutes %= 60;
+			}
+			display("will be", findHours, findMinutes);
+		}
+		else {
+			int findHours = (LocalDateTime.now().getHour() - hours) % 12;
+			int findMinutes = (LocalDateTime.now().getMinute() - minutes);
+			
+			if(findMinutes < 0) {
+				findHours--;
+				findMinutes = 60 - Math.abs(findMinutes);
+			}
+			display("was", findHours, findMinutes);
+		}
+		
+	}
+	
+	private void display(String message, int hour, int min) {
+		
+		JOptionPane.showMessageDialog(null, "The time " + message + " " + hour + ":" + min);
+		
+	}
+	
+	private class ClickListener implements ActionListener{
+		
+		public void actionPerformed(ActionEvent e){
+			
+			if(e.getSource() == forward) {
+				findTime("forward");
+			}
+			else if(e.getSource() == backward) {
+				findTime("backward");
+			}
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		new calculate();
+	}
+	
+}
---
+++ TimeCalculator/src/test.java
@@ -0,0 +1,13 @@
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class test {
+
+	public static void main(String[] args) {
+
+		System.out.println(LocalDateTime.now().getHour());
+		System.out.println(LocalDateTime.now().getMinute());
+	}
+
+}
